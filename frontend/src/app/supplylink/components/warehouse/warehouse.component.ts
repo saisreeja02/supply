368ebc2926f46bda515e951c45fa40bfa2fd51be
username <wecp-dummy@gmail.com>
2025-01-27 11:55:25 +0000

    Day 19

--- frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,5 +1,15 @@
+import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+
+@Component ({
+    selector : 'app-warehouse',
+    template : './warehouse.component.html',
+    styleUrls : ['./warehouse.component.scss']
+})
 
 export class WarehouseComponent {
- 
+
+    
+    constructor (private fb : FormBuilder) {}
 
 }
